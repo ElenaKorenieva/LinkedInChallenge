5cd6b1354f24b8c25c2623d05791d4062950e4d7
ElenaKorenieva <olena.korenieva@gmail.com>
2024-02-16 23:23:38 +0100

    fixed styles, created gitignore

--- linkedin/core/forms.py
+++ linkedin/core/forms.py
@@ -37,8 +37,12 @@ class SignupForm(UserCreationForm):
         fields = ('username','email', 'password1', 'password2', 'jobtitle')
 
         # username = forms.CharField(widget=forms.TextInput(attrs={
-        # 'placeholder': 'Your username',
-        # 'class': 'w-full py-4 px-6 rounded-xl'
+        #     'placeholder': 'Your username',
+        #     'class': 'w-full py-4 px-6 rounded-xl h-2'
+        # }))
+        # email = forms.CharField(widget=forms.EmailInput(attrs={
+        #     'placeholder': 'Your email address',
+        #     'class': 'w-full py-4 px-6 rounded-xl'
         # }))
         # password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         #     'placeholder': 'Your password',
@@ -49,33 +53,34 @@ class SignupForm(UserCreationForm):
         #     'class': 'w-full py-4 px-6 rounded-xl'
         # }))
         # jobtitle = forms.CharField(widget=forms.TextInput(attrs={
-        # 'placeholder': 'Enter your job title',
-        # 'class': 'w-full py-4 px-6 rounded-xl'
+        #     'placeholder': 'Enter your job title',
+        #     'class': 'w-full py-4 px-6 rounded-xl'
         # }))
+
+        INPUT_CLASSES = 'h-8 w-full py-4 px-6 rounded-xl border'
 
         widgets = {
             'username': forms.TextInput(attrs={
                 'placeholder': 'Your username',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                'class': INPUT_CLASSES
             }),
             'email': forms.EmailInput(attrs={
                 'placeholder': 'Your email address',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                'class': INPUT_CLASSES
                 }),
             'password1': forms.PasswordInput(attrs={
                 'placeholder': 'Enter your password',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                'class': INPUT_CLASSES
                 }),
             'password2': forms.PasswordInput(attrs={
                 'placeholder': 'Repeat your password',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                'class': INPUT_CLASSES
                 }),
             'jobtitle': forms.TextInput(attrs={
                 'placeholder': 'Enter your job title',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                'class': INPUT_CLASSES
             }),
         }
-
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
